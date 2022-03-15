bf8f40fd4c29309473cd38a98ccdbfb9da4ca3c4
Nisha Bhavsar <101039209+nisha-1712@users.noreply.github.com>
2022-03-15 12:48:58 +0530

    while declaring variable value has changed

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,7 +3,7 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-        int data = 89;//instance variable
+        int data = 99;//instance variable
 
     }
 }
